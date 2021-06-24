26b1da40073b8d0d8393044e40f6081491490bb6
stevy-sys <stevyralambomanana@gmail.com>
2021-06-24 20:26:49 +0200

    refactoring

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,34 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 function App() {
-	
-  return (
-    <div className="App">
-    <form onSubmit= {(e) => login(e)}> <br/><br/>
-    	<label> Adresse email </label>
-    	<input type="email" id="email" /> <br/><br/>
-    	<label> Mot de passe </label> 
-    	<input type="password" id="password" /> <br/><br/>
-    	<input type="submit" value="Se connecter"/>
-    </form> 
-    </div>
-  );
-}
-function login(e) {
+
+	//state
+	const [mail, setMail] = useState("")
+	const [password, setPassword] = useState("")
+
+	//changement a chaqge tape
+	const handleChangeMail = (e) => {
+		setMail(e.target.value);
+	}
+
+	const handleChangePassword = (e) => {
+		setMail(e.target.value);
+	}
+
+
+	//submit sur click
+	const login = () => {
 		e.preventDefault();
 		let request = {
-			email : document.getElementById('email').value,
-			password : document.getElementById('password').value
+			email: mail,
+			password: password
 		}
-		axios.post('http://localhost:3000/login',request)
-		.then(resp => {
-			alert(resp.data.message);
-		})
-		.catch(err =>{
-			console.log(err);
-		})
+		axios.post('http://localhost:3000/login', request)
+			.then(resp => {
+				alert(resp.data.message);
+			})
+			.catch(err => {
+				console.log(err);
+			})
+	}
+
+	return (
+		<div className="App">
+			<form onSubmit={(e) => login(e)}> <br /><br />
+				<label> Adresse email </label>
+				<input onChange={(e) => handleChangeMail(e)} type="email" /> <br /><br />
+				<label> Mot de passe </label>
+				<input onChange={(e) => handleChangePassword(e)} type="password" /> <br /><br />
+				<input type="submit" value="Se connecter" />
+			</form>
+		</div>
+	);
 }
 
 export default App;
